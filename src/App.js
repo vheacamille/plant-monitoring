210dc4223ea01bf1326c2bf65dcd44f8a9f5b850
Joneaj Luke Bibangco <joneaj.luke.bibangco@accenture.com>
2022-11-08 20:22:06 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import{BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 import HomePage from './pages/HomePage';
 import AllPlants from './components/AllPlants/AllPlants';
+import Navbar from './pages/mainpage/Navbar';
 
 function App() {
   return (
     <Router>
-      <AllPlants/>
+      <HomePage/>
   
       <Routes>
         <Route exact path='/' />
