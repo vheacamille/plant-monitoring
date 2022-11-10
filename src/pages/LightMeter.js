704ed713b555d590012539a8f7336db08e32941a
VheaVicente <vheavicenteT@gmail.com>
2022-11-11 00:46:30 +0800

    Add state and dummy useEffect method for light meter page. Add component for water level page.  Method used inside use effect of water level page is a dummy method as well.

--- src/pages/LightMeter.js
+++ src/pages/LightMeter.js
@@ -1,4 +1,28 @@
+import { useState, useEffect } from 'react';
+import { getDatabase, ref, onValue } from "firebase/database";
+import firebaseDb from '../components/Database/firebaseDbConfig';
+
 const LightMeter = () => {
+    const [lightMeter, setLightMeter] = useState('0');
+
+    // VCV: TEST ONLY. Replace with correct firebase
+    useEffect(() => {
+        const getLightMeter = async () => {
+            const db = getDatabase(firebaseDb);
+            const lightMeterRef = await ref(db, '/WaterLevel/WATER_LEVEL');
+            onValue(lightMeterRef, (snapshot) => {
+                if(snapshot.exists()) {
+                    setLightMeter(JSON.stringify(snapshot.val()));
+                }
+                else {
+                    setLightMeter('0');
+                }
+            })
+        }
+
+        getLightMeter();
+    }, [])
+
     return (
         <div className='light-meter'>
             <h1>Light Meter Page</h1>
--- src/pages/WaterLevel.js
+++ src/pages/WaterLevel.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react';
 import { getDatabase, ref, onValue } from "firebase/database";
+import LiquidChart from 'react-liquidchart';
 import firebaseDb from '../components/Database/firebaseDbConfig';
 
 const WaterLevel = () => {
 
-    const [waterLevel, setWaterLevel] = useState('NA');
+    const [waterLevel, setWaterLevel] = useState('0');
     
     useEffect(() => {
         const getWaterLevel = async () => {
@@ -23,13 +24,36 @@ const WaterLevel = () => {
         getWaterLevel();
     }, [])
 
-
-    
     return (
         <div className='water-level'>
-            { waterLevel !== 'NA' && (<h1>Water Level: {waterLevel}</h1>) }
+            <LiquidChart
+                responsive
+                legend="Water"
+                value={parseFloat(waterLevel)}
+                showDecimal
+                amplitude={4}
+                frequency={2}
+                animationTime={2000}
+                animationWavesTime={2250}
+                postfix="%"
+                legendFontSize={0.1}
+                gradient={{
+                    type: 1,
+                    x1: 0,
+                    x2: 0,
+                    y1: 100,
+                    y2: 0,
+                    stops,
+                }}
+            />
         </div>
     )
 }
+
+const stops = [
+    <stop key={1} stopColor="someColor1" offset="5%" />,
+    <stop key={2} stopColor="someColor2" offset="50%" />,
+    <stop key={3} stopColor="someColor3" offset="85%" />,
+  ];
 
 export default WaterLevel
